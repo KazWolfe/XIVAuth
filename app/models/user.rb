2496b03db04b9fe8247c7a998194cdf1e49e62a3
Kaz Wolfe <root@kazwolfe.io>
2023-04-06 22:30:14 -0700

    Simple Devise Implementation
    - Very simple start with Devise

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :confirmable, :trackable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[discord github]
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root "marketing#index"
+  root 'marketing#index'
+  
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
 end
