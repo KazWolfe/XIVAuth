306e8585fbff6497422b6c921e2f28c58a204493
Kaz Wolfe <root@kazwolfe.io>
2023-05-23 01:18:14 -0700

    Rename OAuthAppsHelper to ClientAppsHelper

--- app/helpers/developer/client_apps_helper.rb
+++ app/helpers/developer/client_apps_helper.rb
@@ -1,4 +1,4 @@
-module Developer::OAuthAppsHelper
+module Developer::ClientAppsHelper
   def app_submit_path(application)
     application.persisted? ? developer_application_path(application) : developer_applications_path
   end
