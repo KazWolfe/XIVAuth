e3f6b64f8080f9c2c22b10b0c271aa4c36225c92
Kaz Wolfe <root@kazwolfe.io>
2023-05-28 12:38:41 -0700

    Fix CharacterRegistration cleanup job
    Properly namespaces the job and actually references the proper classes.

---
+++ app/jobs/character_registration/cleanup_stale_records_job.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CharacterRegistration::CleanupStaleRecordsJob < ApplicationJob
+  queue_as :cronjobs
+
+  def perform(*)
+    do_cleanup
+  end
+
+  def do_cleanup(cutoff = 7.days.ago)
+    CharacterRegistration.where(verified_at: nil)
+                         .where(created_at: ..cutoff)
+                         .in_batches(&:delete_all)
+  end
+end
