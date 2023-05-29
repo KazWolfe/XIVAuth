2df7e1d48cbc78bef50eb4dd9d668caad362920d
Kaz Wolfe <root@kazwolfe.io>
2023-05-28 23:15:54 -0700

    Wire in Postmark to Devise
    - Add Postmark sandbox key to dev creds
    - Set Devise to use the UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,10 +2,12 @@ class UserMailer < Devise::Mailer
   include PostmarkRails::TemplatedMailerMixin
   include Devise::Controllers::UrlHelpers
   default from: 'noreply@xivauth.net'
-  
+
   def reset_password_instructions(record, token, opts = nil)
     self.template_model = {
-      username: record.email,
+      email: record.email,
+      action_url: edit_password_url(record, reset_password_token: token),
+      new_reset_url: new_user_password_url,
       link_ttl: Devise.reset_password_within.inspect
     }
 
@@ -13,17 +15,25 @@ class UserMailer < Devise::Mailer
   end
 
   def confirmation_instructions(record, token, opts = nil)
-    reconfirmation_instructions(record, token, opts) and return if record.pending_reconfirmation?
+    if record.pending_reconfirmation?
+      reconfirmation_instructions(record, token, opts)
+      return
+    end
 
     self.template_model = {
-      username: record.email,
-      confirm_url: confirmation_url(record, confirmation_token: token)
+      email: record.email,
+      action_url: confirmation_url(record, confirmation_token: token)
     }
 
     mail to: record.email, postmark_template_alias: 'welcome'
   end
 
   def reconfirmation_instructions(record, token, opts = nil)
-    super.confirmation_instructions(record, token, opts)
+    self.template_model = {
+      email: record.email,
+      action_url: confirmation_url(record, confirmation_token: token)
+    }
+
+    mail to: record.email, postmark_template_alias: 'reconfirmation'
   end
 end
