796f9a0239cf422a9127bdb182507130a71d534c
Kaz Wolfe <root@kazwolfe.io>
2023-06-14 21:48:54 -0700

    Set Sentry environment properly

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.environment = (ENV['APP_ENV'] || Rails.env.to_s).downcase
 
   config.traces_sample_rate = 1.0
   config.profiles_sample_rate = 1.0
