ae21e06194391e212b025887b32248d67ec61fa3
Kaz Wolfe <root@kazwolfe.io>
2023-08-25 11:31:17 -0700

    Switch to non-semantic div for PW strength meter
    - More control over styling

--- app/javascript/controllers/devise/password_strength_controller.js
+++ app/javascript/controllers/devise/password_strength_controller.js
@@ -2,10 +2,14 @@ import {Controller} from "@hotwired/stimulus"
 import zxcvbn from "zxcvbn";
 
 export default class PasswordStrengthController extends Controller {
-    static targets = ["password", "confirm", "strength", "tips", "warning", "crackTime"];
+    static targets = ["password", "confirm", "strength", "tips", "warning", "crackTime", "meter"];
+    static values = {
+        minScore: Number,
+    }
+
 
     connect() {
-        console.debug('Password strength controller connected!');
+        console.debug('Password strength controller connected!', this);
         this.calc();
     }
 
@@ -14,8 +18,11 @@ export default class PasswordStrengthController extends Controller {
 
         let result = zxcvbn(this.passwordTarget.value);
 
-        this.strengthTarget.value = result.score.toString();
-        this.crackTimeTarget.innerText = result.crack_times_display["offline_slow_hashing_1e4_per_second"];
+        this.crackTimeTarget.innerText = `${result.crack_times_display["offline_slow_hashing_1e4_per_second"]} (${result.guesses_log10})`;
+
+        let scoreClass = this.scoreClass(result.score);
+        this.meterTarget.className = this.meterTarget.className.replace(/\bbg-.+\b/, scoreClass);
+        this.meterTarget.style['width'] = `${this.strengthPercentage(result.guesses_log10)}%`;
 
         this.tipsTarget.innerHTML = "";
         if (result.feedback.warning) {
@@ -35,12 +42,27 @@ export default class PasswordStrengthController extends Controller {
     }
 
     onConfirm() {
-        if (this.confirmTarget.value === "") return;
+        if (this.confirmTarget.value === "") {
+            if (this.passwordTarget.value === "") {
+                this.confirmTarget.classList.remove('is-invalid');
+            }
+
+            return;
+        }
 
         if (this.passwordTarget.value !== this.confirmTarget.value) {
             this.confirmTarget.classList.add('is-invalid');
         } else {
             this.confirmTarget.classList.remove('is-invalid');
         }
+    }
+
+    scoreClass(score) {
+        if (score === 4 || (score > this.minScoreValue)) return "bg-success";
+        return (score < this.minScoreValue) ? "bg-danger" : "bg-warning";
+    }
+
+    strengthPercentage(guessCount) {
+        return Math.min(guessCount * 10, 100);
     }
 }
