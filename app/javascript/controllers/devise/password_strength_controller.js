a6602b3162e4462ed6399766d423f59b03e4f784
Kaz Wolfe <root@kazwolfe.io>
2023-08-25 15:32:35 -0700

    Tweak password strength capping
    Strength meter will be at 50% with a password of score 3, encouraging stronger passwords.

--- app/javascript/controllers/devise/password_strength_controller.js
+++ app/javascript/controllers/devise/password_strength_controller.js
@@ -7,9 +7,19 @@ export default class PasswordStrengthController extends Controller {
         minScore: Number,
     }
 
+    static PASSWORD_DESCRIPTION = {
+        0: "Dangerously Weak",
+        1: "Very Weak",
+        2: "Weak",
+        3: "Acceptable",
+        4: "Strong"
+    }
 
     connect() {
         console.debug('Password strength controller connected!', this);
+
+        this.minScoreValue = 3;
+
         this.calc();
     }
 
@@ -18,11 +28,14 @@ export default class PasswordStrengthController extends Controller {
 
         let result = zxcvbn(this.passwordTarget.value);
 
-        this.crackTimeTarget.innerText = `${result.crack_times_display["offline_slow_hashing_1e4_per_second"]} (${result.guesses_log10})`;
+        this.crackTimeTarget.innerText = `${result.crack_times_display["offline_slow_hashing_1e4_per_second"]}`;
 
         let scoreClass = this.scoreClass(result.score);
+        let strengthValue = this.strengthPercentage(result.guesses_log10)
+
         this.meterTarget.className = this.meterTarget.className.replace(/\bbg-.+\b/, scoreClass);
-        this.meterTarget.style['width'] = `${this.strengthPercentage(result.guesses_log10)}%`;
+        this.meterTarget.style['width'] = `${strengthValue}%`;
+        this.meterTarget.setAttribute('aria-valuenow', Math.floor(strengthValue).toString())
 
         this.tipsTarget.innerHTML = "";
         if (result.feedback.warning) {
@@ -63,6 +76,7 @@ export default class PasswordStrengthController extends Controller {
     }
 
     strengthPercentage(guessCount) {
-        return Math.min(guessCount * 10, 100);
+        // calculated such that a password of score 3 (10^8) will be in the middle.
+        return Math.min(guessCount * (1 / 16) * 100, 100);
     }
 }
