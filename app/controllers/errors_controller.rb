ae53ab7722cecd4f1fb9649ef476d28491b0aab1
Kaz Wolfe <root@kazwolfe.io>
2024-04-29 23:18:25 -0700

    Add better error handler

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,24 @@
+class ErrorsController < ApplicationController
+  skip_before_action :authenticate_user!
+  layout 'error'
+
+  def show
+    @exception = request.env['action_dispatch.exception']
+    @trace_id = trace_id
+    
+    @status_code = @exception.try(:status_code) ||
+      ActionDispatch::ExceptionWrapper.new(
+        request.env, @exception
+      ).status_code
+
+    render 'errors/generic', status: @status_code and return unless template_exists? "errors/#{@status_code}"
+
+    render template: "errors/#{@status_code}", status: @status_code
+  end
+
+  private
+
+  def trace_id
+    Sentry.get_current_scope&.get_span&.trace_id || request.uuid
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Show nicer error messages
+  config.exceptions_app = lambda { |env|
+    ErrorsController.action(:show).call(env)
+  }
 end
