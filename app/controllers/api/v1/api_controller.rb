aafa423533b8969615c34deb388fce18fcec691a
Kaz Wolfe <root@kazwolfe.io>
2024-04-29 22:12:30 -0700

    Add some more Sentry context
    - Track user IDs for UI
    - Track app IDs and context for API

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -4,20 +4,39 @@ class Api::V1::ApiController < ActionController::API
   # There are no "open" API calls; everything must require at least authorization.
   before_action :doorkeeper_authorize!
   before_action :load_token
+  before_action :set_sentry_context, if: proc { Rails.env.production? }
 
   respond_to :json
 
   def current_user
+    return unless doorkeeper_token[:resource_owner_type] == "User"
+
     @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
   end
 
   private
 
   def check_resource_owner_presence
-    render status: :unauthorized unless (current_user.present? && current_user.persisted?)
+    render status: :unauthorized unless current_user.present? && current_user.persisted?
   end
 
   def load_token
     @doorkeeper_token = doorkeeper_token
+  end
+
+  def set_sentry_context
+    return unless doorkeeper_token
+
+    ctx = {
+      application_id: doorkeeper_token.application_id,
+      scopes: doorkeeper_token.scopes
+    }
+
+    if current_user.present?
+      Sentry.set_user(id: current_user.id)
+      ctx[:user_id] = current_user.id
+    end
+
+    Sentry.set_context('oauth_application', ctx)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_sentry_context, if: proc { Rails.env.production? }
+
+  private
+
+  def set_sentry_context
+    Sentry.set_user(id: current_user.id) if user_signed_in?
+  end
 end
