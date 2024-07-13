9252b1905b2a7440bff7e78b87249f0d17ef2030
Kaz Wolfe <root@kazwolfe.io>
2024-07-13 13:13:06 -0400

    Add Pagy to some more pages
    - Change some words around to make more sense.

--- app/controllers/admin/client_applications_controller.rb
+++ app/controllers/admin/client_applications_controller.rb
@@ -1,8 +1,10 @@
 class Admin::ClientApplicationsController < Admin::AdminController
+  include Pagy::Backend
+
   before_action :set_app, only: %i[show update destroy]
 
   def index
-    @client_applications = OAuth::ClientApplication.order(created_at: :desc)
+    @pagy, @client_applications = pagy(OAuth::ClientApplication.order(created_at: :desc))
   end
 
   def show; end
