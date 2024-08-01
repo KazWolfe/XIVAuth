0d5f36d42d646b5b7d9515664d4d323376fa9bd4
Kaz Wolfe <root@kazwolfe.io>
2024-08-01 16:08:48 -0700

    fix regex issue with trailing slash for lodestone url

--- app/helpers/character_registrations_helper.rb
+++ app/helpers/character_registrations_helper.rb
@@ -1,7 +1,7 @@
 module CharacterRegistrationsHelper
   include Pagy::Frontend
 
-  LODESTONE_URL_REGEX = %r{((https?://)?(?<region>[a-z]{2})\.finalfantasyxiv\.com/lodestone/character/)?(?<lodestone_id>\d+)}
+  LODESTONE_URL_REGEX = %r{((https?://)?(?<region>[a-z]{2})\.finalfantasyxiv\.com/lodestone/character/)?(?<lodestone_id>\d+)/?}
 
   def extract_id(id_or_url)
     if (res = LODESTONE_URL_REGEX.match(id_or_url))
