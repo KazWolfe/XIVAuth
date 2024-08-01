b2f9962e01264b12b6f379673a49e8a4bdf00b6e
Kaz Wolfe <root@kazwolfe.io>
2024-08-01 16:20:39 -0700

    better regex validation and client-side filtering
    - only allow trailing slashes and data in actual urls

--- app/helpers/character_registrations_helper.rb
+++ app/helpers/character_registrations_helper.rb
@@ -1,6 +1,10 @@
 module CharacterRegistrationsHelper
   include Pagy::Frontend
 
+  # A stricter regex meant for validation on the browser side, where other constraints are at play.
+  LODESTONE_CLIENT_FILTER_REGEX = %r{^((\d+)|(https?://)?[a-z]{2}\.finalfantasyxiv\.com/lodestone/character/\d+(/.*)?)$}
+
+  # A more relaxed regex variant meant for just grabbing data from things.
   LODESTONE_URL_REGEX = %r{((https?://)?(?<region>[a-z]{2})\.finalfantasyxiv\.com/lodestone/character/)?(?<lodestone_id>\d+)/?}
 
   def extract_id(id_or_url)
