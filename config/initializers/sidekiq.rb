7984155ebfee64421bb5c79e19403bcd63cfa51e
Kaz Wolfe <root@kazwolfe.io>
2024-12-14 19:42:38 -0500

    Heroku Redis uses self-signed certs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,6 +10,8 @@ Sidekiq.configure_server do |config|
     password: ENV.fetch("REDIS_PASSWORD", nil)
   }
 
+  config.redis[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE } if Rails.env.production?
+
   config.on(:startup) do
     if File.exist?((schedule_file = "config/cron.yml"))
       Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
@@ -22,4 +24,6 @@ Sidekiq.configure_client do |config|
     url: "#{ENV['REDIS_URL']}/#{index}",
     password: ENV.fetch("REDIS_PASSWORD", nil)
   }
+
+  config.redis[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE } if Rails.env.production?
 end
