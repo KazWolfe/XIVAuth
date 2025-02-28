7a42e9f811f0d5ec1cf44f8719916e6c473013fd
Kaz Wolfe <root@kazwolfe.io>
2025-02-27 18:16:08 -0800

    fix: Store ED25519 keys as PEM in backend

--- app/models/jwt_signing_keys/ed25519.rb
+++ app/models/jwt_signing_keys/ed25519.rb
@@ -2,32 +2,28 @@ class JwtSigningKeys::Ed25519 < JwtSigningKey
   after_initialize :generate_keypair, if: :new_record?
   validates :public_key, presence: true
 
-  # @return [RbNaCl::Signatures::Ed25519::SigningKey]
-  def private_key
-    RbNaCl::Signatures::Ed25519::SigningKey.new(raw_private_key)
+  def openssl_key
+    @openssl_key ||= OpenSSL::PKey.read(self[:private_key])
   end
 
-  # @param [RbNaCl::Signatures::Ed25519::SigningKey] signing_key
-  def private_key=(signing_key)
-    self[:private_key] = Base64.urlsafe_encode64(signing_key.to_bytes)
-    self[:public_key] = Base64.urlsafe_encode64(signing_key.verify_key.to_bytes)
+  # @return [RbNaCl::Signatures::Ed25519::SigningKey]
+  def private_key
+    @rbnacl_signing_key ||= RbNaCl::Signatures::Ed25519::SigningKey.new openssl_key.raw_private_key
+  end
+
+  # @param [OpenSSL::PKey::PKey] pk
+  def private_key=(pk)
+    self[:private_key] = pk.private_to_pem
+    self[:public_key] = pk.public_to_pem
   end
 
   # @return [RbNaCl::Signatures::Ed2519::VerifyKey]
   def public_key
-    RbNaCl::Signatures::Ed25519::VerifyKey.new raw_public_key
-  end
-
-  def raw_private_key
-    Base64.urlsafe_decode64(self[:private_key])
-  end
-
-  def raw_public_key
-    Base64.urlsafe_decode64(self[:public_key])
+    @rbnacl_public_key ||= RbNaCl::Signatures::Ed25519::VerifyKey.new openssl_key.raw_public_key
   end
 
   def generate_keypair
-    self.private_key = RbNaCl::Signatures::Ed25519::SigningKey.generate
+    self.private_key = OpenSSL::PKey.generate_key("ED25519")
   end
 
   def supported_algorithms
