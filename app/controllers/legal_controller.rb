5b9117a726f8e04e70f7681f94dd9f4c2de2adbb
Kaz Wolfe <root@kazwolfe.io>
2025-09-22 16:39:53 -0700

    tweaks

--- app/controllers/legal_controller.rb
+++ app/controllers/legal_controller.rb
@@ -1,8 +1,14 @@
 class LegalController < ApplicationController
   skip_before_action :authenticate_user!
 
+  layout :set_layout
+
   def terms_of_service; end
   def privacy_policy; end
   def developer_agreement; end
   def security_policy; end
+
+  private def set_layout
+    user_signed_in? ? "portal/page" : "marketing/legal"
+  end
 end
