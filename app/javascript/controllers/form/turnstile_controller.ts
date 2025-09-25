193cefb65d1c4f3f121adf1d2093eb420212cc47
Kaz Wolfe <root@kazwolfe.io>
2025-09-25 13:17:02 -0700

    turnstile: no async, no defer

--- app/javascript/controllers/form/turnstile_controller.ts
+++ app/javascript/controllers/form/turnstile_controller.ts
@@ -2,6 +2,8 @@ import {Controller} from "@hotwired/stimulus";
 
 export default class TurnstileController extends Controller<HTMLDivElement> {
     connect() {
+        console.log("Turnstile connected.");
+
         if (window.turnstile && this.element.innerHTML.trim() === "") {
             window.turnstile.render(this.element);
         }
