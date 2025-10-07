70f438370f2468853a2fb15093c71a0da93c2933
Kaz Wolfe <root@kazwolfe.io>
2025-10-07 08:51:05 -0700

    add discord support link to a few places

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -63,7 +63,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    stored_location_for(resource) || character_registrations_path
+    character_registrations_path
   end
 
   # The path used after sign up for inactive accounts.
