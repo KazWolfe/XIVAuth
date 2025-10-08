311ea2f23cec0623049193d286d7f96dd61aea56
Kaz Wolfe <root@kazwolfe.io>
2025-10-08 16:54:51 -0700

    minimally useful debug interface for apps

--- app/controllers/admin/client_applications_controller.rb
+++ app/controllers/admin/client_applications_controller.rb
@@ -1,15 +1,23 @@
 class Admin::ClientApplicationsController < Admin::AdminController
   include Pagy::Backend
 
-  before_action :set_app, only: %i[show update destroy]
+  before_action :set_app, only: %i[show destroy]
 
   def index
-    @pagy, @client_applications = pagy(OAuth::ClientApplication.order(created_at: :desc))
+    @pagy, @client_applications = pagy(ClientApplication.order(created_at: :desc))
   end
 
   def show; end
 
+  def destroy
+    if @client_application.destroy
+      redirect_to admin_client_applications_path, notice: "Client application deleted."
+    else
+      redirect_to admin_client_application_path(@client_application), alert: "Client application could not be deleted."
+    end
+  end
+
   private def set_app
-    @client_application = OAuth::ClientApplication.find(params[:id])
+    @client_application = ClientApplication.find(params[:id])
   end
 end
