c2e14a78a72e72a9eb129ca7db9f8fcd409c0513
Kaz Wolfe <root@kazwolfe.io>
2025-10-09 10:02:28 -0700

    prod deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_sentry_context, if: proc { Rails.env.production? }
 
+  before_action :redirect_to_new_domain
+
   private def set_sentry_context
     Sentry.set_user(id: current_user.id) if user_signed_in?
+  end
+
+  private def redirect_to_new_domain
+    if request.host == "edge.xivauth.net"
+      redirect_to "#{request.protocol}xivauth.net#{request.fullpath}", status: :moved_permanently
+    end
+
+    if request.host == "eorzea.id"
+      redirect_to "#{request.protocol}xivauth.net", status: :moved_temporarily
+    end
   end
 end
