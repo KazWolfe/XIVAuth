e2f02623b515586983eee2472004ac4d116b378d
Kaz Wolfe <root@kazwolfe.io>
2025-10-27 14:53:31 -0700

    fix: ZEITWERK

--- app/models/oauth/device_grant.rb
+++ app/models/oauth/device_grant.rb
@@ -1,5 +1,5 @@
 class OAuth::DeviceGrant < ApplicationRecord
-  include ::Doorkeeper::DeviceAuthorizationGrant::Orm::ActiveRecord::Mixins::DeviceGrant
+  include ::Doorkeeper::DeviceAuthorizationGrant::DeviceGrantMixin
 
   belongs_to :permissible_policy, class_name: "OAuth::PermissiblePolicy", optional: true
 end
