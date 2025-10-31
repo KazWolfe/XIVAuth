39652c6e59490fe556b707c3ecbfc7b44d37246e
Kaz Wolfe <root@kazwolfe.io>
2025-10-31 15:25:31 -0700

    feat: more blocked display names

--- app/models/user/profile.rb
+++ app/models/user/profile.rb
@@ -4,11 +4,12 @@ class User::Profile < ApplicationRecord
   # TODO: maybe this should be a database table...
   DISPLAY_NAME_BLOCKED_WORDS = [
     "auth", "admin", "root", "system", "oauth", "security", "dalamud", "support", "moderator",
-    /ff[-.@_ ]*(xiv|14)/, /square([-.@_ ]*e)?nix/, /yoshi[-.@_ ]*p/
+    /ff[-.@_ ]*(xiv|14)/, /square([-.@_ ]*e)?nix/, /yoshi[-.@_ ]*p/, /naoki+[-.@_ ]*yoshi(da)?/,
+    /kaz[-.@_ ]*wolfe?/, /goa+ts?/
   ].freeze
 
   validates :display_name, length: { in: 4..32 }, presence: true
-  validates :display_name, format: { with: /\A[ a-zA-Z0-9._\-@]+\z/,
+  validates :display_name, format: { with: /\A[ a-zA-Z0-9._\-]+\z/,
                                      message: "only allows alphanumerics, underscores, spaces, periods, and dashes." }
   validates :display_name, format: { without: /#{Regexp.union(DISPLAY_NAME_BLOCKED_WORDS).source}/i,
                                      message: "cannot be used" }, if: :display_name_changed?
