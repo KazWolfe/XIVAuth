6102d4dec99e027e3fcf7e1cfc1640e4a1f70cb4
Kaz Wolfe <root@kazwolfe.io>
2025-10-31 14:41:06 -0700

    feat: add Running On
    - sneak in a railway referral code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,18 @@ module ApplicationHelper
     ENV["COMMIT_HASH"] || ENV["RAILWAY_GIT_COMMIT_SHA"] || ENV["HEROKU_SLUG_COMMIT"] || nil
   end
 
+  def hosting_provider
+    if ENV["HEROKU_APP_NAME"]
+      :heroku
+    elsif ENV["RAILWAY_SERVICE_ID"]
+      :railway
+    elsif ENV["container"] == "podman"
+      :podman
+    else
+      nil
+    end
+  end
+
   def lower_environment?
     !Rails.env.production? || (ENV["APP_ENV"] != "production")
   end
