d33521d0044e3c50fdd0df6115de5a39518eb532
Kaz Wolfe <root@kazwolfe.io>
2025-10-31 20:58:46 -0700

    feat: new models use UUIDv7

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  before_create :generate_uuidv7
+
+  private def generate_uuidv7
+    return if self.class.attribute_types["id"].type != :uuid ||
+              self.class.columns_hash["id"].default_function != "gen_random_uuid()"
+
+    self.id ||= SecureRandom.uuid_v7
+  end
 end
