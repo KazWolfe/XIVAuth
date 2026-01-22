e24df2a4bcae5d9fb4c95ea043d3bb1097c86020
Kaz Wolfe <root@kazwolfe.io>
2026-01-21 22:16:20 -0800

    bump CI version, allow blobs in img-src

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
+    policy.img_src     :self, :https, :data, :blob
     policy.object_src  :none
     policy.script_src  :self, "https://challenges.cloudflare.com/", "https://static.cloudflareinsights.com/"
     policy.style_src   :self, :https, :unsafe_inline
